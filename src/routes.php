6132cb992eb6e7cbbf3122d94cac5704c2d5198c
Jojopok <zaiajonathan@gmail.com>
2023-11-12 19:30:46 +0100

    fix bug

--- src/routes.php
+++ src/routes.php
@@ -1,16 +1,17 @@
 <?php
+
 session_start();
 // list of accessible routes of your application, add every new route here
-// key : route to match
-// values : 1. controller name
-//          2. method name
-//          3. (optional) array of query string keys to send as parameter to the method
+// key: route to match
+// values: 1. controller name
+//         2. method name
+//         3. (optional) array of query string keys to send as parameter to the method
 // e.g route '/item/edit?id=1' will execute $itemController->edit(1)
 return [
-    '' => ['HomeController', 'index',],
-    'login' => ['UserController', 'login',],
+    '' => ['HomeController', 'index'],
+    'login' => ['UserController', 'login'],
     'logout' => ['UserController', 'logout'],
-    'inscription' => ['InscriptionController', 'validateInscription',],
-    'scenario' => ['ScenarioController','scenario',],
+    'inscription' => ['InscriptionController', 'validateInscription'],
+    'scenario' => ['ScenarioController', 'scenario'],
     'scene' => ['SceneController', 'scene', ['id'], 'planEnigme', ['id']],
 ];
