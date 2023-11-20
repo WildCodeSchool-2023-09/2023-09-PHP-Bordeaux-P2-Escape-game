33a34b0d9d971504cb6712e8280dc58c1352d556
Jojopok <zaiajonathan@gmail.com>
2023-11-20 21:29:21 +0100

    fix bug

--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -17,12 +17,12 @@ class UserManager extends AbstractManager
         return $statement->fetchColumn();
     }
 
-    public function selectOneByEmail(string $email): array
+    public function selectOneByEmail(string $email): ?array
     {
         $statement = $this->pdo->prepare("SELECT * FROM " . static::TABLE . " WHERE email=:email");
         $statement->bindValue(':email', $email, \PDO::PARAM_STR);
         $statement->execute();
-        return $statement->fetch();
+        return $statement->fetch(\PDO::FETCH_ASSOC) ?: null;
     }
 
     public function insert(array $credentials): int
